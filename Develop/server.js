6092762f7036d61a563e983c799902b4bb4b4386
Austin Luper <austin.luper@gmail.com>
2024-03-08 18:29:38 -0500

    Rewrote the server.js file to provide more relevant logic to assignment. Also removed the routing logic since that will be included in the individual routing files for the assignment.

--- Develop/server.js
+++ Develop/server.js
@@ -1,74 +1,17 @@
 const express = require('express');
-const path = require('path');
-const fs = require('fs');
-// Helper method for generating unique ids
-const uuid = require('./helpers/uuid');
-
-const PORT = 3001;
+const apiRoutes = require('./routes/apiRoutes'); // Importing API routes
+const htmlRoutes = require('./routes/htmlRoutes'); // Importing HTML routes
 
 const app = express();
+const PORT = process.env.PORT || 3001; // Setting up the port to listen on, defaulting to 3001 if not provided
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true })); // Middleware to parse urlencoded data
+app.use(express.json()); // Middleware to parse JSON data
+app.use(express.static('public')); // Middleware to serve static files from the public directory
 
-app.use(express.static('public'));
+app.use('/api', apiRoutes); // Mounting the API routes to /api path
+app.use('/', htmlRoutes);   // Mounting the HTML routes to root path
 
-app.get('/', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/index.html'))
-);
-
-// GET request for reviews
-app.get('/api/reviews', (req, res) => {
-  // Send a message to the client
-  res.json(`${req.method} request received to get reviews`);
-
-  // Log our request to the terminal
-  console.info(`${req.method} request received to get reviews`);
+app.listen(PORT, () => {
+  console.log(`App listening at http://localhost:${PORT}`); // Logging a message indicating that the server is running and on which port
 });
-
-// POST request to add a review
-app.post('/api/reviews', (req, res) => {
-  // Log that a POST request was received
-  console.info(`${req.method} request received to add a review`);
-
-  // Destructuring assignment for the items in req.body
-  const { product, review, username } = req.body;
-
-  // If all the required properties are present
-  if (product && review && username) {
-    // Variable for the object we will save
-    const newReview = {
-      product,
-      review,
-      username,
-      upvotes: Math.floor(Math.random() * 100),
-      review_id: uuid(),
-    };
-
-    // Convert the data to a string so we can save it
-    const reviewString = JSON.stringify(newReview);
-
-    // Write the string to a file
-    fs.writeFile(`./db/${newReview.product}.json`, reviewString, (err) =>
-      err
-        ? console.error(err)
-        : console.log(
-            `Review for ${newReview.product} has been written to JSON file`
-          )
-    );
-
-    const response = {
-      status: 'success',
-      body: newReview,
-    };
-
-    console.log(response);
-    res.status(201).json(response);
-  } else {
-    res.status(500).json('Error in posting review');
-  }
-});
-
-app.listen(PORT, () =>
-  console.log(`App listening at http://localhost:${PORT}`)
-);
