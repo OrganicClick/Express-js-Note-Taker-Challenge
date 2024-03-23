4db3ebeadcefd154666a9a67bb50227451aab4c2
Austin Luper <austin.luper@gmail.com>
2024-03-23 15:43:38 -0400

    Added console log and console error logic to the htmlRoutes file for troubleshooting purposes

--- Develop/routes/htmlRoutes.js
+++ Develop/routes/htmlRoutes.js
@@ -4,14 +4,36 @@ const router = require('express').Router();
 
 // Route to serve the index.html file
 router.get('/', (req, res) => {
-  // Sending the index.html file when root URL is accessed
-  res.sendFile(path.join(__dirname, '../public/index.html'));
+  // Construct the file path for index.html
+  const indexPath = path.join(__dirname, '../public/index.html');
+  console.log(`Attempting to send ${indexPath}`);
+  
+  // Send index.html file and handle errors if any
+  res.sendFile(indexPath, (err) => {
+    if (err) {
+      console.error(`Error sending ${indexPath}:`, err);
+      res.status(500).send('Error sending index.html');
+    } else {
+      console.log(`Successfully sent ${indexPath}`);
+    }
+  });
 });
 
 // Route to serve the notes.html file
 router.get('/notes', (req, res) => {
-  // Sending the notes.html file when /notes URL is accessed
-  res.sendFile(path.join(__dirname, '../public/notes.html'));
+  // Construct the file path for notes.html
+  const notesPath = path.join(__dirname, '../public/notes.html');
+  console.log(`Attempting to send ${notesPath}`);
+  
+  // Send notes.html file and handle errors if any
+  res.sendFile(notesPath, (err) => {
+    if (err) {
+      console.error(`Error sending ${notesPath}:`, err);
+      res.status(500).send('Error sending notes.html');
+    } else {
+      console.log(`Successfully sent ${notesPath}`);
+    }
+  });
 });
 
 module.exports = router;
